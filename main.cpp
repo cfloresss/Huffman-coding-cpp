b5d95fae940a7d09b9d8f94806f68b0f8400e803
cfloresss <chfl0801@gmail.com>
2026-09-23 14:40:42 -0700

    Add main function to process input file for Huffman
    Implement main function for Huffman coding application

---
+++ main.cpp
@@ -0,0 +1,30 @@
+// * Filename : main.cpp
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Huffman.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  
+  if (argc != 2) {
+    cerr << "Usage error, expected: ./a.out *.txt\n";
+    exit(1);
+  }
+  
+  string input_file = argv[1];
+  Huffman h(input_file);
+  h.BuildHuffmanTree();
+  h.PrintCharFrequencies(); 
+  h.SetBitCodes();
+  h.SetBitsPerChar();
+  h.Stats();
+  h.BitMap();
+  
+  return 0;
+}
+
+
